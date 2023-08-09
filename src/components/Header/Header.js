c67142dc139b50c2bb75528edcf5df037a7f2e3d
Raja Luqman Sarwar <luqmanrajput012@gmail.com>
2023-08-09 14:20:18 +0500

    updated resume and phone number

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import '../Header/Header.css'
+import React from "react";
+import "../Header/Header.css";
 const Header = () => {
   return (
     <div id="header">
-            <div className="container">
-                <div className="header__text">
-                    <h1>Raja Luqman Sarwar</h1>
-                    <p>Hi, I'am a Web Devloper</p>
-                </div>
-            </div>
+      <div className="container">
+        <div className="header__text">
+          <h1>Raja Luqman Sarwar</h1>
+          <p>Hi, I'am a Web Devloper</p>
         </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
